246d373e84b4537ea3b9ea37d101564a43d172b7
icaroalmeida-somma <icaro.almeida@somma-it.com>
2025-01-30 20:36:07 -0300

    feat: added tailwind css

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,5 +2,7 @@ import { Slot } from 'expo-router';
 import '../styles/global.css';
 
 export default function Layout() {
+  //renderizar as interfaces as rotas através do Slot
+  //pega todas as rotas e interfaces e renderiza aqui no Slot
   return <Slot />;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,24 +1,11 @@
-import { Link, router } from 'expo-router';
-import { Button, Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default function Home() {
-  function goToRouterTest() {
-    router.push('/test');
-  }
-
   return (
     <View className="flex flex-1 bg-[#131016] p-6">
-      <Text className="text-[24px] text-[#fdfcfe] font-bold mt-12">Nome do Evento</Text>
+      <Text className="text-[24px] text-white font-bold mt-12">Nome do Evento</Text>
       <Text className="text-[16px] text-[#6b6b6b]">Sexta, 4 de Novembro de 2022.</Text>
-      <Link
-        className="text-[#fdfcfe] text-[16px] mt-24"
-        href="./test">
-        ir para rota test
-      </Link>
-      <Button
-        title="Ir para rota test"
-        onPress={goToRouterTest}
-      />
+      <TextInput className="h-[56px] bg-[#1f1e25] text-[#fdfcfe] rounded-md" />
     </View>
   );
 }
